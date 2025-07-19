c4ea440b15888ec596637d8ed06dad5f61e801b1
benp1991 <benpassmore91@gmail.com>
2025-07-19 19:03:03 +0100

    please work

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -5,3 +5,10 @@ import { defineAuth } from '@aws-amplify/backend';
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
 
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+    
+  groups: ["ADMINS", "EDITORS"],
+});
